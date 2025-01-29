94c7bab05b39423f5945558093523cf75b5b639a
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-01-29 21:54:28 +0500

    Add env file

--- dist/server.js
+++ dist/server.js
@@ -3,9 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 const node_http_1 = __importDefault(require("node:http"));
+const PORT = process.env.PORT || 5000;
 const server = node_http_1.default.createServer((req, res) => {
     var _a, _b;
     let reqUrl = (_a = req.url) === null || _a === void 0 ? void 0 : _a.trim().toLowerCase();
     let reqMethod = (_b = req.method) === null || _b === void 0 ? void 0 : _b.trim().toUpperCase();
+});
+server.listen(PORT, () => {
+    console.log(`Server successfully initialized on port ${PORT} 😊`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+import { config } from 'dotenv';
+config();
 import http from 'node:http';
+const PORT:number | string = process.env.PORT || 5000;
 
 const server = http.createServer((req, res)=>{
     let reqUrl:string = (req.url as string)?.trim().toLowerCase();
     let reqMethod:string = (req.method as string)?.trim().toUpperCase();
+});
+
+server.listen(PORT, ()=>{
+    console.log(`Server successfully initialized on port ${PORT} 😊`);
 })
